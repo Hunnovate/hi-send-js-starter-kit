e01faabad232f2ef1b6e9b05d1721816230e211d
Bankole Emmanuel <bankoleayobami01@gmail.com>
2025-05-02 18:57:15 +0100

    wip

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   baseUrl: "https://pay.hunnovate.com/api/v1",
-  apiKey: "RgXcb38Bv1L9O5qOQSIwkUVk",
-  projectId: "01jt8vbw40jw5mt4bapff7n0zq"
+  apiKey: "5fkdGe4mipoyLH7Y1pvISWx3",
+  projectId: "01jt91vab73f15jg3n7jarr9aj"
 };
